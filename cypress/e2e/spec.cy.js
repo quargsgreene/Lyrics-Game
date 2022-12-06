91c343b47a3c5629735279f0d39221068b8b070c
Quargs Greene <quargsgreene@gmail.com>
2022-12-06 04:03:11 -0800

    tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,8 @@ describe('Lyrics Game browser tests', () => {
     cy.visit('https://quargsgreene.github.io/lyrics-game/');
   });
 
-  it('displays the intro message' () => {
-    cy.get('#sentence').contains('dove', {timeout: 10000});
+  it('displays the intro message', () => {
+    cy.get('#sentence').contains('dove', { timeout: 10000 });
   });
 
   it('displays the correct message with a correct guess', () => {
@@ -13,6 +13,6 @@ describe('Lyrics Game browser tests', () => {
         cy.get('#guess').click();
       }).then(() => {
         cy.get('#feedback').should('have.text', 'No! I also bet that you\'ve never had neon purple belly button lint.');
-      })
+      });
   });
 });
